00c7a9bb80f4bff446d3a4e4ca2539b6a2a81736
rtseng <rtseng@ghgsat.com>
2024-12-10 10:58:20 -0500

    fix auth.config

--- 1-learnnextjs/nextjs-dashboard/auth.config.ts
+++ 1-learnnextjs/nextjs-dashboard/auth.config.ts
@@ -4,8 +4,8 @@ import type { NextAuthConfig } from "next-auth";
 export const authConfig = {
   //providers: [...],
   pages: {
-    signIn: "/auth/login",
-    error: "/auth/error",
+    signIn: "/login",
+    error: "/error",
   },
   session: { strategy: "jwt" },
 
--- 1-learnnextjs/nextjs-dashboard/auth.ts
+++ 1-learnnextjs/nextjs-dashboard/auth.ts
@@ -10,7 +10,7 @@
 
 
 import NextAuth from 'next-auth';
-import { authConfig } from './auth.congif'; 
+import { authConfig } from './auth.config'; 
 import Credentials from 'next-auth/providers/credentials';
 import { z } from 'zod';
 import { User } from './app/lib/definitions';
