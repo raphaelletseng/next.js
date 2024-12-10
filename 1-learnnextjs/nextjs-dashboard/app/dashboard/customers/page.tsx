5c943d703882a2164ad5104033100d0bd4bade16
rtseng <rtseng@ghgsat.com>
2024-12-10 15:34:55 -0500

    purpleify

--- 1-learnnextjs/nextjs-dashboard/app/dashboard/customers/page.tsx
+++ 1-learnnextjs/nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,11 +1,18 @@
 import { Metadata } from 'next';
+import { lusitana } from '@/app/ui/fonts';
 
 export const metadata: Metadata = {
     title: 'Customers',
 };
 
 export default function Page() {
-    return <p>Customers Page</p>;
+    return (
+        <main>
+            <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+                Customers
+            </h1>
+        </main>
+    );
 }
 
 
