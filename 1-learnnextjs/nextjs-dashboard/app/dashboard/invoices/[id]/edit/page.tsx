c223a6bdca6c19eb67965aff9290f34f75ac80f6
rtseng <rtseng@ghgsat.com>
2024-12-10 07:44:54 -0500

    chapt 16 yip seo

--- 1-learnnextjs/nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
+++ 1-learnnextjs/nextjs-dashboard/app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,11 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+    title: 'Edit Invoice',
+};
  
 export default async function Page(props: {params: Promise<{id: string}>}) {
     const params = await props.params;
@@ -10,7 +15,7 @@ export default async function Page(props: {params: Promise<{id: string}>}) {
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
-    
+
     if (!invoice) {
         return notFound();
     }
